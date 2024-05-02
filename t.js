17092af7b278701116d1a2c7df817b676075c67f
Nedyarg <65202623+Nedyarg@users.noreply.github.com>
2024-05-02 19:33:58 -0400

    Update t.js

--- t.js
+++ t.js
@@ -1,5 +1,13 @@
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
-        	eval(prompt("Eval:"));
-    }
-})
+		var t = window.open("", "_blank", "width=500,height=300");
+		var e = t.document.createElement("iframe");
+		(e.src = "//inglan2.github.io/uRun/popup.html"),
+			(e.style.cssText = "width:100%; height:100%; border:none;"),
+			t.document.body.appendChild(e),
+			t.document.title = "uRun",
+			t.addEventListener("message", function (e) {
+				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+			});
+	}
+});
